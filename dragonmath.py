e88559cafa00be2d49497a898174a450d4d64b9d
Trippwire <58035903+Trippwire2000@users.noreply.github.com>
2020-05-08 21:27:52 +1000

    Add files via upload
    added level change function, need to fix last piece of cobble not destroy()ing.  make more cobble images

--- dragonmath.py
+++ dragonmath.py
@@ -11,6 +11,7 @@ root.geometry('1000x1000')
 
 score = 0
 wrong = 0
+num_of_wins = 0
 
 numlist = [1,2,3,4,5,6,7,8,9,10,11,12]
 font = 'Dragonlands'
@@ -32,7 +33,6 @@ class Shield:
 		s5 = Shield(root, 795)
 		shield_remaining = [s1, s2, s3, s4, s5]
 
-
 class Dead_dragon:
 	def __init__(self, master, xpos):
 		self.dead_dragon_holder = p.Image.open('media/dead_dragon.jpeg')
@@ -41,10 +41,12 @@ class Dead_dragon:
 		self.dead_dragon_label.image = self.dead_dragon_img
 		self.dead_dragon_label.place(x = xpos, y = 0)
 
-	def apply_dead_dragon():
+	def apply_dead_dragon(num):
 		global shield_remaining
-		d1 = Dead_dragon(root, 300)
 
+		xpos = 260 + (num * 40)
+
+		d1 = Dead_dragon(root, xpos)
 
 class Cobble:
 	def __init__(self, master, ypos, file):
@@ -55,23 +57,66 @@ class Cobble:
 		self.cobble_label.image = self.cobble_img
 		self.cobble_label.place(x = 0, y = ypos)
 
-	def apply_cobble():
+	def apply_cobble(num):
 		global cobble_remaining
-		c1 = Cobble(root, 340, 'media/cobblestone1.jpg')
-		c2 = Cobble(root, 410, 'media/cobblestone2.jpg')
-		c3 = Cobble(root, 480, 'media/cobblestone3.jpg')
-		c4 = Cobble(root, 550, 'media/cobblestone4.jpg')
-		c5 = Cobble(root, 620, 'media/cobblestone5.jpg')
-		c6 = Cobble(root, 690, 'media/cobblestone6.jpg')
-		c7 = Cobble(root, 760, 'media/cobblestone7.jpg')
-		c8 = Cobble(root, 830, 'media/cobblestone8.jpg')
-		c9 = Cobble(root, 900, 'media/cobblestone9.jpg')
-		c10 = Cobble(root, 9700, 'media/cobblestone10.jpg')
-		cobble_remaining = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10]
+		if num == 0:
+			cobble_list = ['media/slices/cobblestone0.jpg', 'media/slices/cobblestone1.jpg', 'media/slices/cobblestone2.jpg', 'media/slices/cobblestone3.jpg', 'media/slices/cobblestone4.jpg', 'media/slices/cobblestone5.jpg', 'media/slices/cobblestone6.jpg', 'media/slices/cobblestone7.jpg', 'media/slices/cobblestone8.jpg', 'media/slices/cobblestone9.jpg']
+		elif num == 1:
+			cobble_list = ['media/slices/gold0.jpg', 'media/slices/gold1.jpg', 'media/slices/gold2.jpg', 'media/slices/gold3.jpg', 'media/slices/gold4.jpg', 'media/slices/gold5.jpg', 'media/slices/gold6.jpg', 'media/slices/gold7.jpg', 'media/slices/gold8.jpg', 'media/slices/gold9.jpg']
+
+		c0 = Cobble(root, 340, cobble_list[0])
+		c1 = Cobble(root, 410, cobble_list[1])
+		c2 = Cobble(root, 480, cobble_list[2])
+		c3 = Cobble(root, 550, cobble_list[3])
+		c4 = Cobble(root, 620, cobble_list[4])
+		c5 = Cobble(root, 690, cobble_list[5])
+		c6 = Cobble(root, 760, cobble_list[6])
+		c7 = Cobble(root, 830, cobble_list[7])
+		c8 = Cobble(root, 900, cobble_list[8])
+		c9 = Cobble(root, 9700, cobble_list[9])
+		cobble_remaining = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9]
+
+class Dragon:
+	def __init__(self, master, file):
+		self.dragon_bg_holder = p.Image.open(file)     #images must be 500x700
+		self.dr_bg = ptk.PhotoImage(self.dragon_bg_holder)
+		self.dragon = tk.Label(master, image = self.dr_bg)
+		self.dragon.image = self.dr_bg
+		self.dragon.place(x=0, y=340)
+
+	def apply_dragon(num):
+		global d1
+		dragon_list = ['media/dr/dragon0.jpg', 'media/dr/dragon1.jpg', 'media/dr/dragon2.jpg', 'media/dr/dragon3.jpg', 'media/dr/dragon4.jpg', 'media/dr/dragon5.jpg', 'media/dr/dragon6.jpg', 'media/dr/dragon7.jpg', 'media/dr/dragon8.jpg', 'media/dr/dragon9.jpg']
+		d1 = Dragon(root, dragon_list[num])
+
+class Bg:
+	def __init__(self, master, file):
+		self.bg_holder = p.Image.open(file).crop((0,500,1000,800))
+		self.bg_img = ptk.PhotoImage(self.bg_holder)
+		self.bg_label = tk.Label(master, image = self.bg_img) #text = str(numa) + ' x ' + str(numb), fg = 'white', compound = tk.CENTER, font = (font, 150))
+		self.bg_label.image = self.bg_img
+		self.bg_label.place(x=0, y=40)
 
 
-score_label = tk.Label(root, font = (font, 20), text = 'SCORE      '+str(score))
-score_label.place(x= 40, y=0)
+	def apply_bg(num):
+		global bg1
+		bg_list = ['media/bg/bg0.jpg', 'media/bg/bg1.jpg', 'media/bg/bg2.jpg', 'media/bg/bg3.jpg', 'media/bg/bg4.jpg', 'media/bg/bg5.jpg', 'media/bg/bg6.jpg', 'media/bg/bg7.jpg', 'media/bg/bg8.jpg', 'media/bg/bg9.jpg']
+		bg1 = Bg(root, bg_list[num])
+
+
+def setup(num):
+	playsound('media/dead_dragon.mp3')
+	playsound('media/fanfare.mp3')
+
+	bg1.bg_label.destroy()
+	Bg.apply_bg(num)
+
+	d1.dragon.destroy()
+	Dragon.apply_dragon(num)
+	Cobble.apply_cobble(num)
+
+	problem()
+
 
 def game_over_func():
 	game_over_window = tk.Toplevel()
@@ -92,18 +137,21 @@ def game_over_func():
 
 def guess(a):
 	
-	global score, wrong, question, answer, ans
+	global score, wrong, question, answer, ans, num_of_wins
 	try:
 		if int(answer.get()) == ans:
-			playsound('media/roar.mp3')
 			score += 1
 			score_label.configure(text = 'SCORE      '+str(score))
-			question.destroy()
 			answer.delete(0,'end')
 
 			if len(cobble_remaining) == 1:
-				Dead_dragon.apply_dead_dragon()
+				num_of_wins += 1
+				cobble_remaining[0].cobble_label.destroy()
+				Dragon.apply_dragon(num_of_wins)
+				Dead_dragon.apply_dead_dragon(num_of_wins)
+				setup(num_of_wins)
 			else:
+				playsound('media/roar.mp3')
 				random_cobble = choice(cobble_remaining)
 				random_cobble.cobble_label.destroy()
 				cobble_remaining.remove(random_cobble)
@@ -131,36 +179,20 @@ def problem():
 
 	ans = numa * numb
 
-	question_bg_holder = p.Image.open('media/bg/bg1.jpg').crop((0,500,1000,800))
-	bg = ptk.PhotoImage(question_bg_holder)
-	question = tk.Label(root, image = bg, text = str(numa) + ' x ' + str(numb), fg = 'white', compound = tk.CENTER, font = (font, 150))
-	question.image = bg
-	question.place(x=0, y=40)
-
-	t= 15
-
-	timer_holder = p.Image.open('media/bg/bg1.jpg')
-	timer_img = ptk.PhotoImage(timer_holder)
-	timer = tk.Label(root, font = (font, 40), text = str(t))
-	timer.image = timer_img
-	timer.place(x=500, y=860)
+	bg1.bg_label.configure(text = str(numa) + ' x ' + str(numb), fg = 'white', compound = tk.CENTER, font = (font, 150))
 
 
-	def timer_function():
-		global timer, t
 
-		while t > 0:
-			root.after(1000, lambda: timer.config(text = str(t)))
-			t -= 1
+score_label = tk.Label(root, font = (font, 20), text = 'SCORE      '+str(score))
+score_label.place(x= 40, y=0)
 
-	timer_function()
+Shield.apply_shield()
+Bg.apply_bg(0)
 
 
-dragon_bg_holder = p.Image.open('media/dr/dragon1.jpg')     #images must be 500x700
-dr_bg = ptk.PhotoImage(dragon_bg_holder)
-dragon = tk.Label(root, image = dr_bg)
-dragon.image = dr_bg
-dragon.place(x=0, y=340)
+Dragon.apply_dragon(0)
+Cobble.apply_cobble(0)
+
 
 
 knight_bg_holder = p.Image.open('media/knight.jpg')     #images must be 500x700
@@ -173,11 +205,12 @@ answer = tk.Entry(font = (font, 50), width = 3, justify = 'center')
 answer.place(x = 660, y = 770)
 answer.focus()
 
-root.bind('<Return>', guess)
 
 
-Cobble.apply_cobble()
-Shield.apply_shield()
+
+root.bind('<KP_Enter>', guess)
+
+
 
 problem()
 
